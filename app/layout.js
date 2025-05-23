5ea8ed9c4a30c75da2ba1799b487f941e3d9799b
RanaJyotika <srrana956@gmail.com>
2025-05-23 23:55:14 +0530

    landing page

--- app/layout.js
+++ app/layout.js
@@ -5,7 +5,6 @@ import Header from "@/components/Header";
 import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
 
-
 const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "NextRoleAI",
@@ -14,18 +13,15 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    
     <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${inter.className}`}
-      >
+      <body className={`${inter.className}`}>
         <ClerkProvider
-    publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
-    appearance={{
-      baseTheme: dark,
-    }}
-    >
-         <ThemeProvider
+          publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+          appearance={{
+            baseTheme: dark,
+          }}
+        >
+          <ThemeProvider
             attribute="class"
             defaultTheme="dark"
             enableSystem
@@ -35,14 +31,13 @@ export default function RootLayout({ children }) {
             <Header />
             <main className="min-h-screen">{children}</main>
             {/* footer */}
-            <footer className="bg-muted/50 py-12">
+            <footer className="bg-muted/50 py-2">
               <div className="container mx-auto px-4 text-center text-gray-200">
-              <p>Made with 💗 by Jyotika</p>
+                <p>Made with 💗 by Jyotika</p>
               </div>
             </footer>
-           
           </ThemeProvider>
-    </ClerkProvider>
+        </ClerkProvider>
       </body>
     </html>
   );
